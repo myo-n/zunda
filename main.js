afbabc220ab45ab1ed347202a8d52e6c61a96320
myo-n <99881030+myo-n@users.noreply.github.com>
2023-05-22 16:18:25 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+"use strict";
+
+window.onload = (e)=>{
+	const scene = document.querySelector("a-scene");
+
+	const sound01 = document.getElementById("my_sound01");
+	const btn01 = document.getElementById("my_btn01");
+	btn01.addEventListener("click", (e)=>{
+		const sound = sound01.components.sound;
+		if(sound.isPlaying){
+			sound.pauseSound();
+			e.target.setAttribute("src", "#stop");
+		}else{
+			sound.playSound();
+			e.target.setAttribute("src", "#start");
+		}
+	});
+}
